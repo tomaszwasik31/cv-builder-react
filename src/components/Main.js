cc2e14b9ffd63fd7dd2f689e9d2ece7d061a34ac
Tomasz Wasik <tomaszwasik31@gmail.com>
2023-04-18 15:55:38 +0100

    work history logic

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import phImg from "../components/ph-img.jpg";
 
 export default function Main() {
-  const [formData, setFormData] = React.useState({
+  const [formData, setFormData] = useState({
     firstName: "tom",
     lastName: "was",
     email: "tom@was.com",
@@ -11,74 +11,116 @@ export default function Main() {
     github: "www.link.com",
     description: "short description of your work experience",
     photo: phImg,
+    workHistory: [
+      {
+        workspace: "first work",
+        startDate: "01.01.2020",
+        endDate: "01.01.2020",
+      },
+      {
+        workspace: "second work",
+        startDate: "01.01.2020",
+        endDate: "01.01.2020",
+      },
+    ],
   });
 
-  function handleChange(event) {
-    // Extract the name, value, and files properties from the event target
+  const handleChange = (event) => {
     const { name, value, files } = event.target;
     setFormData((prevData) => ({
       ...prevData,
-
-      // Use a conditional (ternary) operator to determine the value of the property
       [name]: name === "photo" ? URL.createObjectURL(files[0]) : value,
     }));
-  }
+  };
+
+  const handleWorkChange = (index, field, value) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      workHistory: prevData.workHistory.map((work, i) =>
+        i === index ? { ...work, [field]: value } : work
+      ),
+    }));
+  };
+
+  const allWork = () => {
+    return formData.workHistory.map((work, index) => (
+      <div key={index}>
+        <h4>Work {index + 1}</h4>
+        <input
+          type="text"
+          placeholder="Workspace"
+          value={work.workspace}
+          onChange={(e) => handleWorkChange(index, "workspace", e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Start Date"
+          value={work.startDate}
+          onChange={(e) => handleWorkChange(index, "startDate", e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="End Date"
+          value={work.endDate}
+          onChange={(e) => handleWorkChange(index, "endDate", e.target.value)}
+        />
+      </div>
+    ));
+  };
 
   return (
     <div className="main-container">
       <form className="form-container">
         <div className="form-section-container">
           <h3 className="info-title">Personal information</h3>
-
           <input
             type="text"
             placeholder="First Name"
-            onChange={handleChange}
             name="firstName"
             value={formData.firstName}
+            onChange={handleChange}
           />
           <input
             type="text"
             placeholder="Last Name"
-            onChange={handleChange}
             name="lastName"
             value={formData.lastName}
+            onChange={handleChange}
           />
           <input
             type="email"
             placeholder="Email"
-            onChange={handleChange}
             name="email"
             value={formData.email}
+            onChange={handleChange}
           />
           <input
             type="tel"
             placeholder="Phone number"
-            onChange={handleChange}
             name="tel"
             value={formData.tel}
+            onChange={handleChange}
           />
           <input
             type="url"
             placeholder="Linkedin"
-            onChange={handleChange}
             name="linkedin"
             value={formData.linkedin}
+            onChange={handleChange}
           />
           <input
             type="url"
             placeholder="Github"
-            onChange={handleChange}
             name="github"
             value={formData.github}
-          />
-
-          <textarea
-            value={formData.description}
-            placeholder="Description"
             onChange={handleChange}
+          />
+          <textarea
+            placeholder="Description"
             name="description"
+            value={formData.description}
             className="description"
+            onChange={handleChange}
           />
           <label>
             Select JPG file:
@@ -92,6 +134,7 @@ export default function Main() {
         </div>
         <div className="form-section-container">
           <h3 className="info-title">Work History</h3>
+          {allWork()}
         </div>
       </form>
       <div className="preview-container">
