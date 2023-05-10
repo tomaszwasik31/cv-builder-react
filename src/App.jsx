b28569853a7d77e8419d673c5fc51f3bc2ceea16
tomaszwasik31 <80346266+tomaszwasik31@users.noreply.github.com>
2023-05-10 11:22:15 +0100

    localStorage functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,23 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
-import { nanoid } from "nanoid";
-import phImg from "./components/ph-img.jpg";
 
 function App() {
-  const [workHistory, setWorkHistory] = React.useState([
-    {
-      id: nanoid(),
-      workspace: "first work -position",
-      startDate: "march 2020",
-      endDate: "june 2021",
-    },
-    {
-      id: nanoid(),
-      workspace: "second -position",
-      startDate: "march 2020",
-      endDate: "june 2021",
-    },
-  ]);
+  const [formData, setFormData] = React.useState(
+    JSON.parse(localStorage.getItem("formData")) || []
+  );
 
-  const [formData, setFormData] = React.useState({
-    firstName: "tom",
-    lastName: "was",
-    jobPosition: "front-end Developer",
-    email: "tom@was.com",
-    tel: "123123123",
-    linkedin: "www.link.com",
-    github: "www.link.com",
-    description:
-      "short description of your work experience, short description of your work experience, short description of your work experience",
-    photo: phImg,
-  });
+  const [workHistory, setWorkHistory] = React.useState(
+    JSON.parse(localStorage.getItem("workHistory")) || []
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem("workHistory", JSON.stringify(workHistory));
+  }, [workHistory]);
+
+  React.useEffect(() => {
+    localStorage.setItem("formData", JSON.stringify(formData));
+  }, [formData]);
 
   return (
     <div className="App">
